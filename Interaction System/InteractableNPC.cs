690e62a72601048e0d950fbec5bd9c98659e1786
berkayw <berkayw737@gmail.com>
2025-03-28 12:52:49 +0300

    Interactable System Scripts

--- Interaction System/InteractableNPC.cs
+++ Interaction System/InteractableNPC.cs
@@ -11,12 +11,13 @@ public class InteractableNPC : Interactable
         base.Start();
         animator = GetComponent<Animator>();
     }
+    
     protected override void Interaction()
     {
         base.Interaction();
         print("Hello! Unfortunately I don't have a dialog system yet.");
         animator.SetTrigger("Wave");
-
+        
         //Start Dialogue System
     }
 }
